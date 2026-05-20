b92309f2ce7c4ff3c9bf3a2875911c838e7d6b22
eisbaer8 <lars12341@gmx.de>
2026-05-20 22:19:41 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ const routeZurueck = [
 // ⭐ BUTTON-EVENTS ⭐
 // ---------------------------------------------------------
 document.getElementById("hin").addEventListener("click", () => {
-  starteRoute(routeHin, "green");   // Hinweg = grün
+  starteRoute(routeHin, "white");   // Hinweg = grün
 });
 
 document.getElementById("zurueck").addEventListener("click", () => {
